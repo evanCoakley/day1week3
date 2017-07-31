1f66f8eafc992213007af893d8833d98fcf13de3
Evan L Coakley <evancoakley1@gmail.com>
2017-07-31 15:17:42 -0500

    progress

--- main.js
+++ main.js
@@ -82,19 +82,36 @@ let formData = [
   }
 ];
 
-// HINTS:
-// As you can see, we access the first element in the array
-// with [0] and then grab the property "label" using the "." operator
-( function(){
-  // Select the first element from the array
-  let first = formData[ 0 ];
-  // Log the first object
-  console.log( first );
-  // Log the string "First Name"
-  console.log( first.label );
-} )();
+// // HINTS:
+// // As you can see, we access the first element in the array
+// // with [0] and then grab the property "label" using the "." operator
+// (function () {
+//   // Select the first element from the array
+//   let first = formData[0];
+//   // Log the first object
+//   console.log(first);
+//   // Log the string "First Name"
+//   console.log(first.label);
+// })();
 
 
 // -------- Your Code Goes Below this Line --------
+
+function buildForm(){
+let container = document.querySelector("fields") 
+  for(let i = 0; i < formData.length; i++) {
+    if (formData[i].type ==="text"){
+      let input =document.createElement("input");
+      input.type = formData[i].type;
+      input.placeholder = formData[i].labe;
+      container.appendChild(input)
+    }
+    else if (formData[i].type ==="email"){
+      let input = document.createElement("input")
+    }
+  }
+
+
+}
 
 
